6bc04cb87f9e501bd5c6c75a82b31bc1c9cc6c9b
theryanclausen <theryanclausen@gmail.com>
2018-12-11 13:21:30 -0700

    notesview

---
+++ bubbles/src/components/NotesView.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+
+const NotesView = ()=> {
+  const [notes, setNotes] = useState([])
+
+  const fetchNotes = async() =>{
+    const fetchedNotes = await Axios.get('https://backend-project-week-lambda.herokuapp.com/api/notes')
+    setNotes(fetchedNotes.data)
+  }
+
+  useEffect(()=>{fetchNotes()})
+  
+    return (
+      <div>
+        {notes.map(note => <h1 key={note.id}>{note.title}</h1>)}
+
+      </div>
+    );
+  }
+
+
+export default NotesView;
